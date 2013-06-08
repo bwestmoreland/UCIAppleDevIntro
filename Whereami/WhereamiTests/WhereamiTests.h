a442e0f36858c0103f6823f1668798af07637668
bwestmoreland <git@westmoreland.us>
2013-06-08 19:21:30 -0400

    Whereami initial commit

---
+++ Whereami/WhereamiTests/WhereamiTests.h
@@ -0,0 +1,13 @@
+//
+//  WhereamiTests.h
+//  WhereamiTests
+//
+//  Created by Brent Westmoreland on 6/8/13.
+//  Copyright (c) 2013 Brent Westmoreland. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface WhereamiTests : SenTestCase
+
+@end
---
+++ Whereami/WhereamiTests/WhereamiTests.m
@@ -0,0 +1,32 @@
+//
+//  WhereamiTests.m
+//  WhereamiTests
+//
+//  Created by Brent Westmoreland on 6/8/13.
+//  Copyright (c) 2013 Brent Westmoreland. All rights reserved.
+//
+
+#import "WhereamiTests.h"
+
+@implementation WhereamiTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in WhereamiTests");
+}
+
+@end
