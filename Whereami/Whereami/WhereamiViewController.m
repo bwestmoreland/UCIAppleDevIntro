68858ee867b27f7e6cf3db455823c58628bfd111
bwestmoreland <git@westmoreland.us>
2013-06-09 16:37:40 -0400

    Complete Ch04 silver and bronze challenges.

--- Whereami/Whereami/WhereamiViewController.m
+++ Whereami/Whereami/WhereamiViewController.m
@@ -9,6 +9,7 @@
 #import "WhereamiViewController.h"
 
 @interface WhereamiViewController()
+<CLLocationManagerDelegate>
 
 @property (strong, nonatomic) CLLocationManager *locationManager;
 
@@ -20,11 +21,52 @@
 {
     if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]){
         _locationManager = [[CLLocationManager alloc] init];
+        _locationManager.delegate = self;
         _locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+        
+        //Ch04 Bronze Challenge
+        _locationManager.distanceFilter = 50.;
         [_locationManager startUpdatingLocation];
+        
+        //Ch04 Silver Challenge P1
+        if ([CLLocationManager headingAvailable]){
+            [_locationManager startUpdatingHeading];
+        }
     }
     return self;
 }
 
+#pragma mark CLLocationManagerDelegate
+
+
+//Ch04 Silver Challenge P2
+- (void)locationManager:(CLLocationManager *)manager
+       didUpdateHeading:(CLHeading *)newHeading
+{
+    DLog(@"New Heading: %@", newHeading);
+}
+
+- (void)locationManager:(CLLocationManager *)manager
+     didUpdateLocations:(NSArray *)locations
+{
+    DLog(@"Locations: %@", locations);
+}
+
+- (void)locationManager:(CLLocationManager *)manager
+       didFailWithError:(NSError *)error
+{
+    DLog(@"Did fail with error: %@", error);
+}
+
+- (void)nilUnsafeUnretainedObjects
+{
+    //These objects are unsafe_unretained and should be removed manually to avoid a memory leak
+    self.locationManager.delegate = nil;
+}
+
+- (void)dealloc
+{
+    [self nilUnsafeUnretainedObjects];
+}
 
 @end
--- Whereami/WhereamiTests/WhereamiViewSpec.m
+++ Whereami/WhereamiTests/WhereamiViewSpec.m
@@ -11,6 +11,7 @@
 #import <CoreLocation/CoreLocation.h>
 
 @interface WhereamiViewController(TestAdditions)
+<CLLocationManagerDelegate>
 
 @property (strong, nonatomic) CLLocationManager *locationManager;
 
@@ -20,7 +21,7 @@ WhereamiViewController *(^whereamiVC)(void) = ^WhereamiViewController *(void){
     WhereamiViewController *tempVC;
     tempVC = [[WhereamiViewController alloc] initWithNibName: @"WhereamiViewController"
                                                       bundle: nil];
-    
+    //do any additional setup
     return tempVC;
 };
 
@@ -30,17 +31,51 @@ SPEC_BEGIN(WhereamiViewSpec)
 describe(@"Where Am I View Controller", ^{
     __block WhereamiViewController *viewController;
     
+    beforeEach(^{ // Occurs before each enclosed "it"
+        viewController = whereamiVC();
+    });
+    
+    afterEach(^{ // Occurs after each enclosed "it"
+        viewController = nil;
+    });
+    
     context(@"after being initialized", ^{
-        beforeEach(^{ // Occurs before each enclosed "it"
-            viewController = whereamiVC();
-        });
-        
-        afterEach(^{ // Occurs after each enclosed "it"
-            viewController = nil;
-        });
         
         it(@"have a location manager", ^{
             [viewController.locationManager shouldNotBeNil];
+            CLLocationAccuracy accuracy = viewController.locationManager.desiredAccuracy;
+            [[theValue(accuracy) should] equal:theValue(kCLLocationAccuracyBest)];
+        });
+        
+        it(@"should be a delegate for the location manager", ^{
+            id <CLLocationManagerDelegate> delegate = viewController.locationManager.delegate;
+            [[viewController should] equal:delegate];
+        });
+    });
+    
+    context(@"as the delegate for a location manager", ^{
+        
+        it(@"should implement locationManager: didUpdateLocations", ^{
+            [[theBlock(^{
+                [viewController locationManager: nil didUpdateLocations: nil];
+            }) shouldNot] raise]; //an exception
+        });
+        
+        it(@"should implement locationManager: didFailWithError", ^{
+            [[theBlock(^{
+                [viewController locationManager: nil didFailWithError: nil];
+            }) shouldNot] raise]; //an exception
+        });
+        
+        it(@"should implement locationManager: didUpdateHeading", ^{
+            [[theBlock(^{
+                [viewController locationManager: nil didUpdateHeading: nil];
+            }) shouldNot] raise]; //an exception
+        });
+        
+        it(@"should set the locationmanager's delegate to nil on dealloc", ^{
+        //TODO: learn how to actually verify with Kiwi that an unsafe_unretained object is removed in dealloc
+            
         });
     });
 });
