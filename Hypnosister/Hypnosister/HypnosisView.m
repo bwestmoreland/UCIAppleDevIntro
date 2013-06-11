8194a17cf7795626d2c552b005618a7824e5e079
bwestmoreland <git@westmoreland.us>
2013-06-11 17:14:00 -0400

    Implement Hypnois swirls, text and shadows

--- Hypnosister/Hypnosister/HypnosisView.m
+++ Hypnosister/Hypnosister/HypnosisView.m
@@ -30,14 +30,38 @@
     center.x = bounds.origin.x + bounds.size.width / 2.0;
     center.y = bounds.origin.y + bounds.size.height / 2.0;
     
-    float maxRadius = hypot(bounds.size.width, bounds.size.height) / 4.0;
+    float maxRadius = hypot(bounds.size.width, bounds.size.height) / 2.0;
     
     CGContextSetLineWidth(context, 10);
     
-    CGContextSetRGBStrokeColor(context, 0.6, 0.6, 0.6, 1.0);
+    [[UIColor lightGrayColor] setStroke];
     
-    CGContextAddArc(context, center.x, center.y, maxRadius, 0.0, M_PI * 2., YES);
+    for (float currentRadius = maxRadius; currentRadius > 0; currentRadius -= 20 ) {
+        CGContextAddArc(context, center.x, center.y, currentRadius, 0., M_PI * 2.,  YES);
+        
+        CGContextStrokePath(context);
+    }
     
-    CGContextStrokePath(context);
+    NSString *text = @"You are getting sleepy";
+    
+    UIFont *font = [UIFont boldSystemFontOfSize: 28];
+    
+    CGRect textRect;
+    
+    textRect.size = [text sizeWithFont: font];
+    
+    textRect.origin.x = center.x - textRect.size.width / 2.0;
+    textRect.origin.y = center.y - textRect.size.height / 2.0;
+    
+    [[UIColor blackColor] setFill];
+    
+    CGSize offset = CGSizeMake(4, 3);
+    
+    CGColorRef color = [[UIColor darkGrayColor] CGColor];
+    
+    CGContextSetShadowWithColor(context, offset, 2.0, color);
+    
+    [text drawInRect: textRect
+            withFont: font];
 }
 @end
