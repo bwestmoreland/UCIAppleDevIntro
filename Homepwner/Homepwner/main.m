17667631d3bc7b1774023f2878f98704bd69a7d2
bwestmoreland <git@westmoreland.us>
2013-06-15 21:05:34 -0400

    Homepwner initial commit

---
+++ Homepwner/Homepwner/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Homepwner
+//
+//  Created by Brent Westmoreland on 6/15/13.
+//  Copyright (c) 2013 Brent Westmoreland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
